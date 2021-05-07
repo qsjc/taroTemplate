ec30eabcda1f9f1bd75790e51e65396ed300c135
wangjun <wangjun20101120@163.com>
2021-05-07 15:16:36 +0800

    feat: 增加版本更新处理

--- src/app.js
+++ src/app.js
@@ -19,6 +19,28 @@ Taro.pxTransform({
 
 const App = new Vue({
   store,
+  onLaunch () {
+    const updateManager = Taro.getUpdateManager()
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+    })
+    updateManager.onUpdateReady(function () {
+      Taro.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+    updateManager.onUpdateFailed(function () {
+      // 新的版本下载失败
+    })
+  },
   onShow (options) {
   },
   render(h) {
